d90aa0e2fd0bf27224998f789dd60e150ace7087
kepler155c@gmail.com <kepler155c@gmail.com>
2019-06-28 14:22:03 -0400

    cleanup

--- sys/autorun/upgraded.lua
+++ sys/autorun/upgraded.lua
@@ -5,3 +5,7 @@ if fs.exists('sys/etc/app.db') then fs.delete('sys/etc/app.db') end
 if fs.exists('sys/extensions') then fs.delete('sys/extensions') end
 if fs.exists('sys/network') then fs.delete('sys/network') end
 if fs.exists('startup') then fs.delete('startup') end
+
+if fs.exists('sys/autorun/gps.lua') then fs.delete('sys/autorun/gps.lua') end
+if fs.exists('sys/apps/network/redserver.lua') then fs.delete('sys/apps/network/redserver.lua') end
+if fs.exists('sys/apis') then fs.delete('sys/apis') end
